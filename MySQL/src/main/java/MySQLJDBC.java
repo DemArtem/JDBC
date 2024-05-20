976c475d80e88400cca8e3c97e304efb2e23c196
gk <demar.demidov@yandex.ru>
2024-05-20 20:14:00 +0300

    test

--- MySQL/src/main/java/MySQLJDBC.java
+++ MySQL/src/main/java/MySQLJDBC.java
@@ -7,10 +7,15 @@ public class MySQLJDBC {
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","");) {
             statement = connection.createStatement();
             statement.setQueryTimeout(30);
+            add();
+            update();
             readAll();
+            updateUniv();
+
         } catch (SQLException e) {
             e.printStackTrace(System.err);
         }
+
     }
 
     public static void readAll() throws SQLException {
@@ -18,5 +23,20 @@ public class MySQLJDBC {
         ResultSet rs = statement.executeQuery("select name from student");
         while (rs.next())
             System.out.printf("name: %s%n", rs.getString("name"));
+    }
+
+    public static void add() throws SQLException {
+        String sql = "INSERT INTO student(name) VALUES ('Zara')";
+        statement.executeUpdate(sql);
+    }
+
+    public static void update() throws SQLException {
+        String sql = "update student set surname='Иванова' WHERE name='Zara'";
+        statement.executeUpdate(sql);
+    }
+
+    public static void updateUniv() throws SQLException {
+        String sql = "update student set surname='Иванова', univ_id=4 WHERE name='Zara'";
+        statement.executeUpdate(sql);
     }
 } 
